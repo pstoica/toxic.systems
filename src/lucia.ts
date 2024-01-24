927d7be0f2471f7025405a5c1ba5d41311157632
Patrick Stoica <patark@icloud.com>
2024-01-24 01:04:18 -0500

    initial commit

---
+++ src/lucia.ts
@@ -0,0 +1,16 @@
+import { lucia } from "lucia";
+import { libsql } from "@lucia-auth/adapter-sqlite";
+// import { createClient } from "@libsql/client";
+
+// const db = createClient({
+// 	url: "file:test/main.db",
+// });
+
+// const auth = lucia({
+// 	adapter: libsql(db, {
+// 		user: "user",
+// 		key: "user_key",
+// 		session: "user_session"
+// 	}),
+//   env: import.meta.env.PROD ? "PROD" : "DEV",
+// });
